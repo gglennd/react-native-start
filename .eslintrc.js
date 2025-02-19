86166c1541186605d6dc7709a2bbd354497eafd7
gglennd <grdimaano18@gmail.com>
2025-02-19 17:46:21 +0800

    refactor: autoformat

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,18 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "prettier"],
-  plugins: ["prettier"],
-  ignorePatterns: ["/dist/*"],
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier'],
+  ignorePatterns: ['/dist/*'],
   rules: {
-    "prettier/prettier": "error",
-  },
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'none',
+        semi: true,
+        singleQuote: true,
+        jsxSingleQuote: true
+      }
+    ],
+    'prefer-template': ['error']
+  }
 };
